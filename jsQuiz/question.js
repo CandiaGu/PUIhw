7749fedcf5816f44868739886e1b47ee444b48d3
CandiaGu <candi37@gmail.com>
2019-10-02 11:26:35 -0400

    added js to quiz

--- jsQuiz/question.js
+++ jsQuiz/question.js
@@ -4,11 +4,11 @@ class Question {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
+  }
 
   isCorrectAnswer(choice){
     return this.answer === choice;
   }
-}
 }
 
 var questions = [
